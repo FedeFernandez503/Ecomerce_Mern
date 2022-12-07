40723ce133034a8a59d5cad20ed2e30868f4d60d
DisarG <80352728+DisarG@users.noreply.github.com>
2022-12-07 17:56:07 -0300

    Add files via upload

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
-import { useNavigate, Navigate} from "react-router";
+import { useNavigate, Navigate } from "react-router";
 import { Link } from "react-router-dom"
 import "./Login.css"
-const Login = ({user, setUser}) => {
+const Login = ({ user, setUser }) => {
     if (user.isLogin) {
-        return <Navigate to={"/user/:_id"}/>
-      }
+        return <Navigate to={"/user/:_id"} />
+    }
     return (
         <div className="container">
-            <h1>Login</h1>
+            <form>
+                <h1>Login</h1>
+                <input placeholder="Email" />
+                <input placeholder="Password" />
+                <button>Ingresar</button>
+            </form>
         </div>
     )
 }
-export default Login ;
+export default Login;
--- front/src/pages/User/User.jsx
+++ front/src/pages/User/User.jsx
@@ -2,7 +2,8 @@ import "./User.css"
 export function User() {
     return (
         <div className="container">
-            
+            <h1>Perfil</h1>
+
         </div>
     )
 }

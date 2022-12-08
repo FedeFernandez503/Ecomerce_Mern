7dafc2f20edef80a3fc950e755f950c1dc46a04e
Juanma5201 <113566705+Juanma5201@users.noreply.github.com>
2022-12-08 02:13:03 -0300

    home y error404

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -2,8 +2,10 @@ import "./Home.css"
 
 export function Home() {
     return (
-        <div className="container">
-            <h3>Matea Libre</h3>
+        <div className="containerHome">
+            <h3 className="h3Home">Bienvenido a Matea Libre</h3>
+            <img className="imgHome" src="https://media.istockphoto.com/id/1328836875/es/vector/dispositivos-electr%C3%B3nicos-realistas-y-gadgets-en-isometr%C3%ADa-ilustraci%C3%B3n-isom%C3%A9trica-vectorial.jpg?s=612x612&w=0&k=20&c=UkqpAly7HK6ObV1bYFRWp7qJQqTimsi9IFPSCwooGXk=" alt="" />
+            <h4 className="h4Home">tu mejor lugar de electronica</h4>
         </div>
     )
 }

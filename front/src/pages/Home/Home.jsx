173eff5b1451ea084468ae913d359412aae6d903
FedeFernandez503 <80352692+FedeFernandez503@users.noreply.github.com>
2022-12-12 00:31:40 -0300

    Update Home.jsx
    actualizacion

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -2,8 +2,11 @@ import "./Home.css"
 
 export function Home() {
     return (
-        <div className="container">
-            <h1>HOME</h1>
+        <div className="containerHome">
+            <div className="text-content">
+            <h2>Bienvenido a Matea Libre</h2>
+            <h2>tu mejor lugar de electronica</h2>
+            </div>
         </div>
     )
 }

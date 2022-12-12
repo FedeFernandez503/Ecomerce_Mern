9d31653354dedf1e4f56772881beaecf8840416f
FedeFernandez503 <80352692+FedeFernandez503@users.noreply.github.com>
2022-12-12 00:33:19 -0300

    Update Error404.jsx
    actualizacion

--- front/src/pages/Error404/Error404.jsx
+++ front/src/pages/Error404/Error404.jsx
@@ -1,8 +1,9 @@
 import "./Error404.css"
 export function Error404() {
     return (
-        <div className="container">
-            <h1>Error404</h1>
+        <div className="containerError">
+            <h2 className="">Error404</h2>
+            <img className="imgError" src="https://media.istockphoto.com/id/881680376/es/foto/p%C3%A1gina-no-encontrada-plantilla-para-sitio-web-reparador-de-juguete-robot-con-llave-ajustable.jpg?b=1&s=170667a&w=0&k=20&c=dTPvOv0ektl1sAzeAXJ8k5yqQuYO-EcIsbdjJP4IWv8=" alt="" />
         </div>
     )
 }
